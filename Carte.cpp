cf97fec4a4f71538fa889e4185b4b2ed4e6f2b50
Bcricholf <flobarflo@gmail.com>
2015-03-22 17:27:15 +0100

    Create Carte.cpp

---
+++ Carte.cpp
@@ -0,0 +1,11 @@
+#include "Carte.h"
+
+using namespace sf;
+using namespace std;
+
+Carte::Carte(int tailleMap, std::string nomCarte)  ///ajouter la position pour faire de l'aléatoire
+{
+
+}
+
+///Pour l'instant le code de la carte est dans le main 
